8e0230acb0dffb43540e7ec8ed289d3847d0d10f
Dineshkumar K <141417723+Dineshkumar245@users.noreply.github.com>
2025-11-11 13:56:45 +0530

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,6 +2,7 @@ import { Code, Database, Palette, Users, Lightbulb, Zap, Goal, Shield} from "luc
 
 const Skills = () => {
   const technicalSkills = [
+    { name: "ServiceNow", icon: fa-cloud }
     { name: "HTML/CSS", icon: Code },
     { name: "JavaScript", icon: Code },
     { name: "Java", icon: Code },
@@ -72,4 +73,5 @@ const Skills = () => {
 };
 
 export default Skills;
+
 
